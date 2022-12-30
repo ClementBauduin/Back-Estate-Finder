db4b97190799d79dae6dbbd869997865dcc6de57
ClementBauduin <103571576+ClementBauduin@users.noreply.github.com>
2022-12-30 21:00:16 +0100

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -15,7 +15,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
-app.use(cors({credentials: true, origin: 'https://www.estate-finder.clementbauduin.com/'}))
+app.use(cors({credentials: true, origin: 'https://www.estate-finder.clementbauduin.com'}))
 app.use(morgan('tiny'))
 app.use(cookieParser())
 app.use(router)
