d6a4a42a432cb7175c7f45107802aaf3a77aafb1
ClementBauduin <103571576+ClementBauduin@users.noreply.github.com>
2022-12-31 18:22:36 +0100

    Update logout.js

--- src/routes/logout.js
+++ src/routes/logout.js
@@ -1,4 +1,7 @@
 export function logout(req, res) {
-    res.clearCookie('accessToken');
+    res.clearCookie('accessToken', {
+        path: "/",
+        domain: ".clementbauduin.com"
+    });
     return res.status(200).json({ message: "User logged out" });
 }
